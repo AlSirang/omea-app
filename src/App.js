4bf89fc707527f0a94fd2763fcf509b25c9531f6
AlSirang <uddinbashir1999@gmail.com>
2023-01-18 21:14:12 +0500

    fix: create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dapp from "pages/dapp";
 import Home from "pages/home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/dapp",
+    element: <Dapp />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
