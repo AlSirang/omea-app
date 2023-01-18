a3970b8eed9c5b5079afbc2a8419c936e4b923b9
AlSirang <uddinbashir1999@gmail.com>
2023-01-18 21:13:47 +0500

    fix: create dapp page file

---
+++ src/pages/dapp.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+import PageLayout from "src/layouts/pageLayout";
+
+export default function Dapp() {
+  return (
+    <PageLayout>
+      <Container>
+        <h1> hello</h1>
+      </Container>
+    </PageLayout>
+  );
+}
