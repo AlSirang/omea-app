1f30d2c8197bd40a388458a884b3a75e666fce27
AlSirang <uddinbashir1999@gmail.com>
2023-01-25 23:32:39 +0500

    fix: add referral data

--- src/components/referral.js
+++ src/components/referral.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import Progress from "components/progressBar";
+import { WalletUserContext } from "src/context";
+import { loadInvestorInfo } from "src/utils/web3.helpers";
 import "src/styles/dapp/referral.css";
 
+const initialState = {
+  isDataLoading: false,
+  totalLocked: 0,
+  startTime: 0,
+  lastCalculationDate: 0,
+  claimableAmount: 0,
+  claimedAmount: 0,
+  referAmount: 0,
+  referCount: 0,
+};
+
 export default function Referral() {
+  const {
+    contextState: { account },
+  } = WalletUserContext();
+
+  const [{ referAmount, referCount }, dispatch] = useReducer(
+    (state, payload) => ({ ...state, ...payload }),
+    initialState
+  );
+
+  const loadReferralData = async () => {
+    try {
+      dispatch({ isDataLoading: true });
+
+      const results = await loadInvestorInfo(account);
+      dispatch({ ...results });
+    } catch (err) {}
+
+    dispatch({ isDataLoading: false });
+  };
+
+  useEffect(() => {
+    account && loadReferralData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [account]);
+
+  const onCopyLink = () => {
+    const link = window.location.href + `?referral=${account}`;
+    navigator.clipboard.writeText(link).then(
+      function () {
+        console.log("success");
+      },
+      function (err) {
+        console.error("Async: Could not copy text: ", err);
+      }
+    );
+  };
+
   return (
     <div>
       <h4 className="heading">Referral</h4>
@@ -13,7 +63,7 @@ export default function Referral() {
           <div className="referral-info-inner">
             <div>
               <h6># Referred</h6>
-              <p>6</p>
+              <p>{referCount}</p>
             </div>
             <div>
               <h6>Bonus</h6>
@@ -21,12 +71,12 @@ export default function Referral() {
             </div>
             <div>
               <h6>Referral Earnings</h6>
-              <p>100.2340,00</p>
+              <p>{referAmount}</p>
             </div>
           </div>
 
           <div className="refferal-button">
-            <button className="btn btn-primary">
+            <button onClick={onCopyLink} className="btn btn-primary">
               <strong>Copy link</strong>
             </button>
           </div>
