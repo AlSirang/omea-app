caac9d763a72fc3e8ff4e626b37ac4d9249fc196
AlSirang <uddinbashir1999@gmail.com>
2023-01-25 19:49:13 +0500

    fix: add context provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dapp from "pages/dapp";
 import Home from "pages/home";
+import { WalletContextProvider } from "src/context";
+import Wrapper from "components/wrapper";
 
 const router = createBrowserRouter([
   {
@@ -14,7 +16,12 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <WalletContextProvider>
+      <Wrapper />
+      <RouterProvider router={router} />
+    </WalletContextProvider>
+  );
 }
 
 export default App;
