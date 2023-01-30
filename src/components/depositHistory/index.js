27ee2b5854324afe740f1d5809ce07cf7e891e97
AlSirang <uddinbashir1999@gmail.com>
2023-01-30 14:25:52 +0500

    fix: add loading in deposit section

--- src/components/depositHistory/index.js
+++ src/components/depositHistory/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useReducer } from "react";
+import LineOfDots from "components/lineOfDots";
+import { RenderDepositHistory } from "./units";
 import { WalletUserContext } from "src/context";
-import "src/styles/dapp/depositHistory.css";
 import { getOmeaContractInstance, getRpcProvider } from "src/utils/constants";
 import { parseDepositHistory } from "src/utils/helpers";
-import { RenderDepositHistory } from "./units";
+import "src/styles/dapp/depositHistory.css";
 
 const initialState = {
   isDataLoading: false,
@@ -11,7 +12,7 @@ const initialState = {
 };
 
 export default function DepositHistory() {
-  const [{ depostInfo }, dispatch] = useReducer(
+  const [{ depostInfo, isDataLoading }, dispatch] = useReducer(
     (state, payload) => ({ ...state, ...payload }),
     initialState
   );
@@ -62,9 +63,16 @@ export default function DepositHistory() {
     <div>
       <h4 className="heading">Deposit History</h4>
       <div className="card-main deposit-card">
+        {!isDataLoading && !depostInfo.length && <h5>No deposit history</h5>}
         {depostInfo.map((props, index) => (
           <RenderDepositHistory {...props} key={index} />
         ))}
+
+        {isDataLoading && (
+          <div className="h-100 d-flex align-items-center">
+            <LineOfDots />
+          </div>
+        )}
       </div>
     </div>
   );
