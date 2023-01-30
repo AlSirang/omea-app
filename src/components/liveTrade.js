c47fb7277aa70c7a1a45928aada1d40b5da2c79e
AlSirang <uddinbashir1999@gmail.com>
2023-01-30 18:02:59 +0500

    fix: render live trades data

--- src/components/liveTrade.js
+++ src/components/liveTrade.js
@@ -1,13 +1,45 @@
+import { useEffect, useReducer } from "react";
 import { Container } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
-import "src/styles/livetrade.css";
+import Papa from "papaparse";
+import { firstNPostiveNumbersAfterDecimal } from "src/utils/constants";
 
+import "src/styles/livetrade.css";
+import LineOfDots from "./lineOfDots";
+
+const initialState = {
+  isDataLoading: false,
+  tradesInfo: [],
+};
 function LiveTradeTable() {
+  const [{ tradesInfo, isDataLoading }, dispatch] = useReducer(
+    (state, payload) => ({ ...state, ...payload }),
+    initialState
+  );
+  useEffect(() => {
+    dispatch({ isDataLoading: true });
+    Papa.parse(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vT11iCZjV8NvQUKPcmcwY723dvls8nJK29YTdGGIRvtlo2M-kR_-2ZiiOdJPIdjlAwZpKsfiKqA50-X/pub?output=csv",
+
+      {
+        download: true,
+        header: true,
+        complete: (results) => {
+          dispatch({ tradesInfo: results.data });
+          dispatch({ isDataLoading: false });
+        },
+        error: (err) => {
+          dispatch({ isDataLoading: false });
+        },
+      }
+    );
+  }, []);
+
   return (
     <Container className="main-container d-md-block d-none">
       <h4 className="heading">Live trades</h4>
-      <div className="card-main card-main-inner">
-        <Table responsive="sm" className="table-main">
+      <div className=" card-main-inner">
+        <Table responsive className="table-main">
           <thead>
             <tr className="table-head">
               <th>Date</th>
@@ -22,49 +54,25 @@ function LiveTradeTable() {
             </tr>
           </thead>
           <tbody>
-            <tr className="table-head">
-              <td>12/01/2023</td>
-              <td>BNB/BUSD</td>
-              <td>121212</td>
-              <td>
-                <button className="btn btn-primary  table-btn-yellow">
-                  active
-                </button>
-              </td>
-              <td>Long</td>
-              <td>294.9852</td>
-              <td>1212</td>
-              <td>20x</td>
-              <td>---</td>
-            </tr>
-            <tr className="table-head">
-              <td>12/01/2023</td>
-              <td>BNB/BUSD</td>
-              <td>121212</td>
-              <td>
-                <button className="btn btn-secondary  table-btn-green">
-                  Won
-                </button>
-              </td>
-              <td>Long</td>
-              <td>294.9852</td>
-              <td>1212</td>
-              <td>20x</td>
-              <td className="end-td-green-data">26%</td>
-            </tr>
-            <tr className="table-head">
-              <td>12/01/2023</td>
-              <td>BNB/BUSD</td>
-              <td>1212121</td>
-              <td>
-                <button className="btn btn-danger  table-btn-red">lost</button>
-              </td>
-              <td>Long</td>
-              <td>294.9852</td>
-              <td>12112</td>
-              <td>20x</td>
-              <td className="end-td-red-data">-3%</td>
-            </tr>
+            {tradesInfo.map((props) => (
+              <TableRow {...props} />
+            ))}
+            {!isDataLoading && !tradesInfo.length && (
+              <tr>
+                <td colspan="9" className="border-b-none">
+                  <h5 className="text-center text-light mt-3">
+                    No Trades found
+                  </h5>
+                </td>
+              </tr>
+            )}
+            {isDataLoading && (
+              <tr>
+                <td colspan="9" className="border-b-none">
+                  <LineOfDots />
+                </td>
+              </tr>
+            )}
           </tbody>
         </Table>
       </div>
@@ -73,3 +81,36 @@ function LiveTradeTable() {
 }
 
 export default LiveTradeTable;
+
+const TableRow = ({
+  Amount,
+  Date,
+  Entry,
+  Exit,
+  Leverage,
+  PNL,
+  Pair,
+  Position,
+  Status,
+}) => {
+  return (
+    <>
+      <tr className="table-head">
+        <td>{Date}</td>
+        <td>{Pair}</td>
+        <td>{firstNPostiveNumbersAfterDecimal(Amount, 2)}</td>
+        <td>
+          <span className={`mb-0 table-btn status-${Status.toLowerCase()}`}>
+            {Status.toLowerCase()}
+          </span>
+        </td>
+        <td>{Position}</td>
+        <td>{firstNPostiveNumbersAfterDecimal(Entry, 2)}</td>
+        <td>{firstNPostiveNumbersAfterDecimal(Exit, 2)}</td>
+
+        <td>{Leverage}</td>
+        <td className={`${Status.toLowerCase()}-pnl`}>{PNL}</td>
+      </tr>
+    </>
+  );
+};
