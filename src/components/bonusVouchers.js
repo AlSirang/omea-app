5c1ae9e6a0dabec3a77f3ec8df75b03df0294b83
AlSirang <uddinbashir1999@gmail.com>
2023-02-03 02:52:17 +0500

    fix: add bonuse section

--- src/components/bonusVouchers.js
+++ src/components/bonusVouchers.js
@@ -1,46 +1,80 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { WalletUserContext } from "src/context";
 import "src/styles/dapp/bonusvouchers.css";
+import { getOmeaContractInstance, getRpcProvider } from "src/utils/constants";
+import { timeConverter } from "src/utils/dateTimeHelper";
+import { parseBonusesInfo } from "src/utils/web3.helpers";
+import LineOfDots from "./lineOfDots";
+
+const initialState = {
+  isDataLoading: false,
+  bonusInfo: [],
+};
 
 export default function Bonusvouchers() {
+  const [{ bonusInfo, isDataLoading }, dispatch] = useReducer(
+    (state, payload) => ({ ...state, ...payload }),
+    initialState
+  );
+
+  const {
+    contextState: { account, ethersProvider },
+  } = WalletUserContext();
+
+  const loadBonusVochers = async () => {
+    try {
+      dispatch({ isDataLoading: true });
+      const contractInstance = getOmeaContractInstance(
+        ethersProvider || getRpcProvider()
+      );
+
+      const bonusOf = await contractInstance.bonusOf(account);
+
+      const results = parseBonusesInfo(bonusOf);
+
+      dispatch({ bonusInfo: results });
+    } catch (err) {
+      console.log({ err });
+    }
+    dispatch({ isDataLoading: false });
+  };
+  useEffect(() => {
+    account && loadBonusVochers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [account]);
   return (
     <div>
       <h4 className="heading"> Bonus vouchers</h4>
       <div className="card-main deposit-card">
-        <div className="deposit-card-head">
-          <div className="deposit-date">
-            <p>Bonus Id: </p>
-          </div>
-        </div>
-        {/* d  h m s */}
-        <div className="withraw-section">
-          <div className="withdraw-duration">
-            <div className="duration-item">
-              <h6>d</h6>
-              <p>0</p>
-            </div>
-            <div className="duration-item">
-              <h6>h</h6>
-              <p>0</p>
-            </div>
-            <div className="duration-item">
-              <h6>m</h6>
-              <p>0</p>
-            </div>
-            <div className="duration-item">
-              <h6>s</h6>
-              <p>0</p>
-            </div>
-          </div>
+        {!isDataLoading && !bonusInfo.length && <h5>No Bonus history</h5>}
 
-          <div className="deposit-amount">
-            <p className="mb-0">12.00 BUSD</p>
+        {bonusInfo.map((props, index) => (
+          <BonusInfo {...props} key={index} />
+        ))}
+
+        {isDataLoading && (
+          <div className="h-100 d-flex align-items-center">
+            <LineOfDots />
           </div>
-          {/* <button className="btn btn-secondary btn-withdraw claim-button">
-            <strong>Claim</strong>
-          </button> */}
-        </div>
-        <hr className="deposit-hr" />
+        )}
       </div>
     </div>
   );
 }
+
+const BonusInfo = ({ amount, createdDate }) => {
+  return (
+    <React.Fragment>
+      <div className="withraw-section mt-2">
+        <div className="withdraw-duration">
+          <p>{timeConverter(createdDate)}</p>
+        </div>
+
+        <div className="deposit-amount">
+          <p className="mb-0">{amount} BUSD</p>
+        </div>
+      </div>
+      <hr className="deposit-hr" />
+    </React.Fragment>
+  );
+};
