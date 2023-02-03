1eb3e9a413144d48e5b95d0b9b63a18065cedff0
AlSirang <uddinbashir1999@gmail.com>
2023-02-03 22:05:17 +0500

    fix: progress bar

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -2,6 +2,7 @@ import BootstrapProgressBar from "react-bootstrap/ProgressBar";
 import { firstNPostiveNumbersAfterDecimal } from "src/utils/constants";
 
 export default function ProgressBar({ now = 0, max = 10, level = 1 }) {
+  console.log({ now });
   return (
     <div className="progressBar-container">
       <h4>level&nbsp; {level}</h4>
@@ -9,7 +10,7 @@ export default function ProgressBar({ now = 0, max = 10, level = 1 }) {
         className="progress-bar"
         variant="warning"
         animated
-        now={now === 0 ? 1 : now}
+        now={now == 0 ? 1 : now}
         max={max}
         label={`${firstNPostiveNumbersAfterDecimal((now / max) * 100, 1)} %`}
       />
